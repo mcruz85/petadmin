8d56df4b945674b7cae9cbbfd05ab20360becfba
Marcus Cruz <marcus.cruz85@gmail.com>
2018-03-08 20:29:20 -0300

    add model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :title, uniqueness: true, presence: true
+  validates :price, presence: true
+
   def fae_display_field
     title
   end
