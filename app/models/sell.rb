afae51e7a79d61ab6ba3ca73866ed3d10f07dd9d
Marcus Cruz <marcus.cruz85@gmail.com>
2018-03-10 15:51:07 -0300

    add models service, product, cliente, campaign

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,9 +1,20 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
 
+  has_many :sell_products
+  has_many :products, through: :sell_products
+
+  has_many :sell_services
+  has_many :services, through: :sell_services
+
+  belongs_to :discount
+  belongs_to :client
+
   validates :client, presence: true
 
   enum status: { finished: 0, canceled: 1 }
+
+  before_save :set_total
 
   def fae_display_field
     id
@@ -13,6 +24,19 @@ class Sell < ApplicationRecord
     order(:id)
   end
 
-  belongs_to :discount
-  belongs_to :client
+  private
+
+ def set_total
+   total = 0
+   self.products.each {|p| total += p.price }
+   self.services.each {|s| total += s.price }
+
+   if self.discount.present?
+     total = total - self.discount.value
+   end
+
+   total = (total >= 0) ? total : 0
+   self.total = total
+ end
+
 end
